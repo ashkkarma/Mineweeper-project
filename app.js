24a781c98d47884bd04ee849fe561183ab9a8ca4
username <youremail@domain.com>
2023-09-20 20:30:03 +0100

    before empty space func

--- app.js
+++ app.js
@@ -45,22 +45,45 @@ function mineMap() {
   })
 }
 mineMap()
+///counter
+
 
 ///set click function
+const flagArr = []
+function logFlag (){
+  console.log(flagArr);
+}
+function counter (){
+  
+  const count = document.querySelector('.mineCtr')
+  const countDis = document.createElement('div')
+  countDis.innerHTML = (40-flagArr.length)
+count.appendChild(countDis)
+}
 
 function flagSelector (){
   let disableRight = document.querySelector('.container')
   disableRight.addEventListener('contextmenu',(right)=>{
     right.preventDefault();
-
+    
+    counter()
   } )
 for (let i = 16 ; i < (cellCount+16) ; i ++){
   let flagSelect = document.getElementById(`${i}`)
+  let flagInd = parseInt(flagSelect.innerText)
   flagSelect.addEventListener('contextmenu',(right)=>{
   right.preventDefault();
   const flagImg = document.createElement('img')
   flagImg.src = 'flag.png'
   flagSelect.appendChild(flagImg)
+  if (flagArr.includes(flagInd)){
+  flagImg.remove()
+    flagArr.splice(flagInd,0)
+  }else{flagArr.push(flagInd)
+    logFlag ()
+    winGame()}
+  
+  
 })
 }
 }
@@ -91,19 +114,78 @@ function reset(){window.location.reload()}
 function emptySpace (){
   
 }
+///timer 
+const watchTime = document.getElementById("time");
+
+const controlButton = document.getElementById("startStop");
+const resetButton = document.getElementById("reset");
+
+let runTime = false;
+let elapsedTime = 0;
+let startTime;
+let interval;
+
+/* adjust the numbers in seconds, minutes and hours */
+function formatTime(milliseconds) {
+  const totalSeconds = Math.floor(milliseconds / 1000); // 1000 per seconds
+  // const hours = Math.floor(totalSeconds / 3600); // seconds after hours
+  const minutes = Math.floor((totalSeconds % 3600) / 60); // 60 minutes to hours
+  const seconds = totalSeconds % 60; // 60 second to minutes
+
+  return `${minutes.toString().padStart(2, "0")}:
+  ${seconds.toString().padStart(2, "0")}`;
+}
+
+/* function for adding the current native time */
+const realtime = () => {
+  const currentTime = Date.now();
+  elapsedTime += currentTime - startTime; // to increase the number over time
+  startTime = currentTime;
+  watchTime.innerHTML = formatTime(elapsedTime);
+};
+
+/* Start & Pause button */
+document.querySelector('.container').addEventListener("click", () => {
+  if (!runTime) {
+    // if start button is clicked it will stop
+    controlButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    if (!startTime) {
+      // if the start button is clicked
+      startTime = Date.now();
+    }
+    interval = setInterval(realtime, 10);
+  } else {
+    // if the pause button is clicked it will resume
+    controlButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+    clearInterval(interval);
+  }
+  runTime = !runTime;
+});
+
+/* Reset buton */
+// resetButton.addEventListener("click", () => {
+//   controlButton.innerHTML = '<i class="fa-solid fa-play"></i>'; // if the reset button is clicked it will return to the start button
+//   clearInterval(interval); // return default time
+//   runTime = false; // fixed time still running after pause
+//   startTime = null; // this too
+//   elapsedTime = 0; // default elapsed time
+//   watchTime.innerHTML = formatTime(elapsedTime);
+// });
 // emptySpace ()
 
 //hint map func
+const hintArr = []
 
 function hint (){
   
   for (let i = 16 ; i < (cellCount+16) ; i ++){
     
+    
     const hintInd = document.getElementById(`${i}`)
-    let num = parseInt(hintInd.innerText)
+    let num = parseInt(hintInd.innerHTML)
     hintInd.addEventListener('click', ()=>{
     if (mineArray.includes(num) === false ){
-      const hintArr = []
+      hintArr.splice(0,hintArr.length)
       if(( num % 16 !== 0) && (num % 16 !== 15)){
       if(mineArray.includes(num - 1) === true){
         hintArr.push(num-1)
@@ -130,12 +212,23 @@ function hint (){
         hintArr.push(num+17)
       }
       
-      if (hintArr.length>0){
+       if (hintArr.length>=1)
+      {
         const hintCellGet = document.getElementById(`${i}`)
         let hintCellCreate = document.createElement('span')
         hintCellCreate.innerHTML = `${hintArr.length}`
         hintCellGet.appendChild(hintCellCreate)
+        // console.log(hintArr);
+       
         }
+        // else if (hintArr.length=0){
+        //   console.log('hiii');
+        //   // const emptyCellGet = document.getElementById(`${i}`)
+        //   // let emptyCellCreate = document.createElement('span')
+        //   // emptyCellCreate.innerHTML = 'empty'
+        //   // emptyCellGet.appendChild(emptyCellCreate)
+        // }
+       
     } else if (( num % 16 === 0)){
       if(mineArray.includes(num - 15) === true){
         hintArr.push(num-15)
@@ -180,7 +273,7 @@ function hint (){
         hintCellCreate.innerHTML = `${hintArr.length}`
         hintCellGet.appendChild(hintCellCreate)
         }
-    }
+    } 
   }
 
 })
@@ -188,12 +281,33 @@ function hint (){
  
 }
 hint()
-/// winning function
+// winning function
 
 function winGame () {
-  
+
+  if (flagArr.length>=40){
+    // const arr1 = ['c', 'b', 'a'];
+// const arr2 = ['a', 'b', 'c'];
+
+function areEqual(array1, array2) {
+  if (array1.length === array2.length) {
+    return array1.every(element => {
+      if (array2.includes(element)) {
+        return true;
+      }
+
+      return false;
+    });
+  }
+
+  return false;
 }
-// winGame ()
+
+console.log(areEqual(flagArr, mineArray)) 
+  }
+
+}
+
 
 
 
@@ -235,3 +349,5 @@ window.addEventListener('Dom', init())
     //       hintArr.push(num-16)
     //     }console.log(hintArr);
     // }
+
+   
